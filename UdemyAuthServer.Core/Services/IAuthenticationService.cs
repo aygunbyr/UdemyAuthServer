7755563df4eb9ffd90432b56850842ab4afe89a9
Aygün Bayır <turkoaygun@gmail.com>
2024-10-16 20:59:31 +0300

    Core Layer

---
+++ UdemyAuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using SharedLibrary.Dtos;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Response<ClientTokenDto> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
---
+++ UdemyAuthServer.Core/Services/IServiceGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using SharedLibrary.Dtos;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IServiceGeneric<TEntity, TDto> 
+        where TEntity : class 
+        where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TEntity>>> GetAllAsync();
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<TDto>> AddAsync(TEntity entity);
+        Task<Response<NoDataDto>> Remove(TEntity entity);
+        Task<Response<NoDataDto>> Update(TEntity entity);
+    }
+}

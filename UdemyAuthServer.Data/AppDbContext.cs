4a111350a780a6d5c5c2876decc53dd0d0e4446d
Aygün Bayır <turkoaygun@gmail.com>
2024-10-17 12:06:32 +0300

    Data Layer

---
+++ UdemyAuthServer.Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UdemyAuthServer.Core.Models;
+
+namespace UdemyAuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        public DbSet<Product> Products;
+        public DbSet<UserRefreshToken> UserRefreshTokens;
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ UdemyAuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.UnitOfWork;
+
+namespace UdemyAuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommmitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

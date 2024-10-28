88109b5bea349e44ccd0f3c1b3d8b485c1beb328
Aygün Bayır <turkoaygun@gmail.com>
2024-10-28 18:52:22 +0300

    Upgrade solution to .NET 8

--- WebApplicationCheck/Program.cs
+++ WebApplicationCheck/Program.cs
@@ -1,17 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using SharedLibrary.Configurations;
-using SharedLibrary.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
-var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
-builder.Services.AddCustomTokenAuth(tokenOptions);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,8 +17,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
